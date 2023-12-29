f5491792b77998017bc619b3ba21218569b79b67
Guillaume Demay <gdemay@blackshark.ai>
2023-12-29 14:05:25 +0100

    Remove unused code and fix export statement

--- src/App.js
+++ src/App.js
@@ -3,25 +3,10 @@ import './App.css';
 import AutoPlayVideo from './AutoPlayVideo';
 
 function App() {
-  const [visitCount, setVisitCount] = useState(0);
-
-  useEffect(() => {
-    // Function to increment and retrieve the visit count
-    const incrementVisitCount = () => {
-      const count = parseInt(localStorage.getItem('visitCount') || 0);
-      localStorage.setItem('visitCount', count + 1);
-      return count + 1;
-    };
-
-    // Update the visit count
-    const updatedCount = incrementVisitCount();
-    setVisitCount(updatedCount);
-  }, []);
 
   return (
     <div className="App">
       <AutoPlayVideo />
-      <p>Page views: {visitCount}</p>
     </div>
   );
 }
